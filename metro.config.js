d04dc9846ac4a39c6a85d76172045a28c01083eb
Marcelo Damasceno <marcelodamasceno@gmail.com>
2024-01-18 21:07:01 -0300

    fix: Altera para config do repositorio

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config')
-
-// module.exports = getDefaultConfig(__dirname)
 
 const config = getDefaultConfig(__dirname)
 
-config.resolver.resolverMainFields.unshift('sbmodern')
+const { transformer, resolver } = config
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer')
+}
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg']
+}
 
 module.exports = config
